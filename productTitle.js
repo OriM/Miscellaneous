59ee1e6b77ef9c697fcf86a30dd371e47e0ac28f
Ori Millo <ori.m@pepperi.com>
2021-03-03 13:31:12 +0200

    added product title

---
+++ productTitle.js
@@ -0,0 +1,5 @@
+
+var productTitle = GetValueByApiName("ItemTSAProductTitleParagraph");
+var externlID = GetValueByApiName("ItemTSAProductTitleParagraph");
+var content = "<h1 class ='MainHeader'>" + productTitle + "</h1>" + externlID + "<br/><br/>";
+return content;
