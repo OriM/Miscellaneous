8edf9d19115da5cc3d25fdf8e926f690c08d57c5
Ori Millo <ori.m@pepperi.com>
2021-03-09 10:58:43 +0200

    applied changes on the js file

--- Winzer/WinzerAttributes.js
+++ Winzer/WinzerAttributes.js
@@ -1,13 +1,7 @@
 var apiNames = [
   { title: "Diameter", apiName: "ItemTSADiameter" },
   { title: "Drive Type", apiName: "ItemTSADriveType" },
-  { title: "Features", apiName: "ItemTSAFeaturesHTML" },
-  { title: "empty", apiName: "" },
-  { title: "empty", apiName: "" },
-  { title: "empty", apiName: "" },
   { title: "Finish", apiName: "ItemTSAFinish" },
-  { title: "empty", apiName: "" },
-  { title: "empty", apiName: "" },
   { title: "Head Style", apiName: "ItemTSAHeadStyle" },
   { title: "Length", apiName: "ItemTSALength" },
   { title: "Material", apiName: "ItemTSAMaterial" },
@@ -18,20 +12,17 @@ var apiNames = [
 ];
 
 var max = 5;
-var apiNamesLenth = apiNames.length();
+var apiNamesLenth = apiNames.length;
 var attributesFound = 0;
 var content = "";
 var i;
 for (i = 0; i < apiNamesLenth && attributesFound < max; i++) {
   var apiName = apiNames[i].apiName;
-  var attirbuteValue = apiName;// GetValueByApiName(apiName);
-  console.log(i + " " + attirbuteValue);
+  var attirbuteValue = GetValueByApiName(apiName);
   if (attirbuteValue != "") {
     var attirbuteTitle = apiNames[i].title;
-    content += "<b>" + +":</b> " + attirbuteValue + "<br/>";
+    content += "<b>" + attirbuteTitle + ":</b> " + attirbuteValue + "<br/>";
     attributesFound++;
-    console.log(attributesFound);
-    console.log(content);
   }
 }
 
