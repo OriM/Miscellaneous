db85e5af612f4a8f5f4edf46b877c32ab7a50bca
Ori Millo <ori.m@pepperi.com>
2021-03-16 15:56:49 +0200

    made attributes appear first

--- Winzer/ProductInfoMobile.js
+++ Winzer/ProductInfoMobile.js
@@ -2,5 +2,5 @@ var description = GetValueByApiName("ItemTSATopTextHTML");
 var attributes = GetValueByApiName("TSATopAttributesHTML");
 var f_s_d = GetValueByApiName("TSAFeaturesSpecificationsDocuments");
 
-var html = description + "</br></br>" + attributes + f_s_d;
+var html = attributes + "</br></br>" + description + f_s_d;
 return html;
