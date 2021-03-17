140fc5c0a0f22795fdec57bf87c50332003ce90b
Ori Millo <ori.m@pepperi.com>
2021-03-17 14:17:46 +0200

    removed unused code

--- Winzer/ProductInfoDesktop.js
+++ Winzer/ProductInfoDesktop.js
@@ -1,10 +1,5 @@
 var title = GetValueByApiName("TSAProductTitle");
 var body = GetValueByApiName("TSAProductInfoMobile");
 
-// var attributes = GetValueByApiName("TSATopAttributesHTML");
-// var description = GetValueByApiName("ItemTSATopTextHTML");
-// var f_s_d = GetValueByApiName("TSAFeaturesSpecificationsDocuments");
-
 var html = title + body;
-// var html = title + "</br></br>" + description + "</br></br>" + attributes + f_s_d;
 return html;
