2fd25fe9c5633f55d6d181dc82826c7034494a3c
Ori Millo <ori.m@pepperi.com>
2023-12-18 13:05:59 +0200

    Dynamic medium View - hide 0 values

--- Nelt/Fields/TSAMediumViewContent.js
+++ Nelt/Fields/TSAMediumViewContent.js
@@ -14,6 +14,76 @@ var Packaging = GetValueByApiName('ItemTSACSQuantity');
 var LastOrder = GetValueByApiName('LastOrderItemDate');
 var Delivered = GetValueByApiName('TSADeliveredQty');
 
+
+var style = `<style>
+.nelt-card-ver {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    text-align: left;
+    gap: 0.5rem;
+}
+.nelt-one-liner {
+    display: flex;
+    flex-direction: row;
+    gap: 0.25rem;
+    width: 100%;
+    padding-inline-start: 0.125rem;
+}
+</style>`;
+
+const div = `<div class="nelt-one-liner">
+                <p style="margin: 0;" class="body-md title ng-star-inserted">{{TITLE}}</p>
+                <p style="margin: 0;" class="body-md bold">{{REPLACE}}</p>
+            </div>`;
+
+var accHtml = '<div>' + style + '<div class="nelt-card-ver">';
+
+accHtml += `<div class="nelt-one-liner">
+<p style="margin: 0;" class="body-md title ng-star-inserted">SKU:</p>
+<p style="margin: 0;" class="body-md bold">{{Sku}}</p>
+</div>`;
+
+if (Price != '0') {
+    var newDiv = div;
+    newDiv = newDiv.replace('{{REPLACE}}', '{{Price}}');
+    newDiv = newDiv.replace('{{TITLE}}', 'Cena:');
+    accHtml += newDiv;
+}
+if (PriceVAT != '0') {
+    var newDiv = div;
+    newDiv = newDiv.replace('{{REPLACE}}', '{{PriceVAT}}');
+    newDiv = newDiv.replace('{{TITLE}}', 'Cena sa PDV-om:');
+    accHtml += newDiv;
+}
+if (Supplies != '0') {
+    var newDiv = div;
+    newDiv = newDiv.replace('{{REPLACE}}', '{{Supplies}}');
+    newDiv = newDiv.replace('{{TITLE}}', 'Zalihe:');
+    accHtml += newDiv;
+}
+
+accHtml += `<div class="nelt-one-liner">
+<p style="margin: 0;" class="body-md title ng-star-inserted">Pakovanje:</p>
+<p style="margin: 0;" class="body-md bold">{{Packaging}}</p>
+</div>`;
+
+if (LastOrder != '0') {
+    var newDiv = div;
+    newDiv = newDiv.replace('{{REPLACE}}', '{{LastOrder}}');
+    newDiv = newDiv.replace('{{TITLE}}', 'Poslednja porudzbina:');
+    accHtml += newDiv;
+}
+if (Delivered != '0') {
+    var newDiv = div;
+    newDiv = newDiv.replace('{{REPLACE}}', '{{Delivered}}');
+    newDiv = newDiv.replace('{{TITLE}}', 'Isporuceno:');
+    accHtml += newDiv;
+}
+
+accHtml += '</div></div>';
+
+
 var html = `<div>
 <style>
     .nelt-card-ver {
@@ -64,7 +134,7 @@ var html = `<div>
 </div>
 `;
 
-var res = html;
+var res = accHtml;// html;
 res = res.replace('{{Price}}', Price );
 res = res.replace('{{Sku}}', Sku );
 res = res.replace('{{PriceVAT}}', PriceVAT );
