c582ca0d8923d53550a42db7485dbe03c0496af1
Yonatan Kof <kof@yonatankof.com>
2023-12-18 11:24:50 +0200

    Improve Medium view

--- Nelt/Fields/TSAMediumViewTitle.js
+++ Nelt/Fields/TSAMediumViewTitle.js
@@ -1,7 +1,22 @@
 var CardTitle = GetValueByApiName('ItemName');
-var html =
-	'<h2 style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;" class="color-user-primary title-4xl">{{CardTitle}}</h2>';
+var html = `
+<span style="height: 100%; display: flex; align-items: center">
+	<h3
+		style="
+			display: -webkit-box;
+			-webkit-line-clamp: 3;
+			-webkit-box-orient: vertical;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			margin: 0;
+		"
+		class="body-3xl title"
+	>
+		{{CardTitle}}
+	</h3>
+</span>
+`;
 
 var res = html;
-res = res.replace('{{CardTitle}}', CardTitle );
+res = res.replace('{{CardTitle}}', CardTitle);
 return res;
